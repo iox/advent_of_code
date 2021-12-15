965ba634410f4f2c64bee44d47251c44ba88d018
Ignacio Huerta <ignacio@ihuerta.net>
2021-12-15 16:01:39 +0100

    2021 day 15: part b

--- 15_code.rb
+++ 15_code.rb
@@ -1,161 +1,146 @@
-#https://gist.github.com/PaulSanchez/a68e2b40af587dc968615f73c446e1e5
-class PriorityQueue
-  attr_reader :elements
 
-  def initialize
-    @elements = [nil]
+
+
+def calculate(grid)
+  queue = []
+  lowest_risks = Hash.new(1000000) # Initialize this with a big value
+
+  # Fill the queue
+  queue << [0,0]
+  lowest_risks[[0, 0]] = 0
+
+
+  loop do
+    break if queue.size == 0
+    
+    y,x = queue.shift
+
+    existing_risk = lowest_risks[[y,x]]
+
+    [[-1,0], [0,-1],[0, 1], [1, 0]].each do |diff_x, diff_y|
+      next_x = x + diff_x
+      next_y = y + diff_y
+      new_point = [next_y, next_x]
+      # Check if the new point is out of bounds
+      next if next_x < 0 || next_y < 0 || next_y >= grid.size || next_x >= grid[0].size
+    
+      new_risk = grid[next_y][next_x]
+
+      if new_risk + existing_risk < lowest_risks[new_point]
+        lowest_risks[new_point] = new_risk + existing_risk
+        queue << new_point
+      end    
+    end
   end
 
-  def <<(element)
-    @elements << element
-    bubble_up(@elements.size - 1)
-  end
-
-  def pop
-    exchange(1, @elements.size - 1)
-    max = @elements.pop
-    bubble_down(1)
-    max
-  end
-
-  private
-
-  def bubble_up(index)
-    parent_index = (index / 2)
-
-    return if index <= 1
-    return if @elements[parent_index][0] >= @elements[index][0]
-
-    exchange(index, parent_index)
-    bubble_up(parent_index)
-  end
-
-  def bubble_down(index)
-    child_index = (index * 2)
-
-    return if child_index > @elements.size - 1
-
-    not_the_last_element = child_index < @elements.size - 1
-    left_element = @elements[child_index]
-    right_element = @elements[child_index + 1]
-    child_index += 1 if not_the_last_element && right_element[0] > left_element[0]
-
-    return if @elements[index][0] >= @elements[child_index][0]
-
-    exchange(index, child_index)
-    bubble_down(child_index)
-  end
-
-  def exchange(source, target)
-    @elements[source], @elements[target] = @elements[target], @elements[source]
-  end
+  lowest_risks[[grid.size-1, grid.first.size-1]]
 end
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 grid = File.readlines('15_input').map{|l| l.chomp.chars.map(&:to_i)}
+# puts calculate(grid)
 
 
 
-# Make a first run
-minimal_risk = 600
-# total_steps = grid.size + grid.first.size - 2
+grid = grid.map.with_index do |line, y|
+  new_line = line.dup
 
-
-queue = PriorityQueue.new
-# min_risk_until_finish, risk, start
-queue << [0, 0, [0,0]]
-
-
-def distance(grid,y,x)
-  grid.size + grid.first.size - x - y
+  4.times.with_index do |i|
+    line.each do |value, x|
+      new_value = value+i+1
+      new_value = (new_value - 9) if new_value > 9
+      new_line.push(new_value)
+    end
+  end
+  new_line
 end
 
-
-lowest_risks = {}
-
-
-index = 0
-loop do
-  index += 1
-  
-  item = queue.pop
-
-  break if item.nil?
-
-  if index % 100000 == 0
-    puts "#{index} loops. #{item.inspect}. Current minimum #{minimal_risk}. Queue size #{queue.elements.size}"
-  end
-
-  y,x = item[2]
-  risk = item[1]
-
-  if risk > minimal_risk
-    # puts "Risk was too high (#{risk})"
-    next
-  end
-
-  # Check the values of the neigbours
-  down = grid[y+1] ? grid[y+1][x] : nil
-  right = grid[y][x+1]
-
-  if down.nil? && right.nil?
-    puts "We have reached the end of this path: #{item.inspect}"
-    if risk < minimal_risk
-      puts "We have found a new minimal risk #{risk}"
-      minimal_risk = risk
+new_grid = grid.dup
+4.times.with_index do |i|
+  grid.each do |line|
+    new_line = line.dup
+    new_line.map! do |value|
+      new_value = value+i+1
+      new_value = (new_value - 9) if new_value > 9
+      new_value
     end
+    new_grid.push(new_line)
   end
+end
 
-  if down
-    risk_min_until_finish = risk + down + distance(grid,y+1,x)
+grid = new_grid
 
-    lowest_risk_recorded = lowest_risks[[y+1,x]] || 10000
-    new_risk = risk+down
 
-    if risk_min_until_finish < minimal_risk && lowest_risk_recorded > risk
-      queue << [
-        risk_min_until_finish,
-        new_risk,
-        [y+1,x]
-      ]
-      lowest_risks[[y+1,x]] = new_risk
-    end
+
+
+# grid.each do |line|
+#   line.each do |value|
+#     print value
+#   end
+#   print "\n"
+# end
+
+
+puts calculate(grid)
+
+
+
+  # if risk > minimal_risk
+  #   # puts "Risk was too high (#{risk})"
+  #   next
+  # end
+
+  # # Check the values of the neigbours
+  # down = grid[y+1] ? grid[y+1][x] : nil
+  # right = grid[y][x+1]
+
+  # if down.nil? && right.nil?
+  #   puts "We have reached the end of this path: #{item.inspect}"
+  #   if risk < minimal_risk
+  #     puts "We have found a new minimal risk #{risk}"
+  #     minimal_risk = risk
+  #   end
+  # end
+
+  # if down
+  #   risk_min_until_finish = risk + down + distance(grid,y+1,x)
+
+  #   lowest_risk_recorded = lowest_risks[[y+1,x]] || 10000
+  #   new_risk = risk+down
+
+  #   if risk_min_until_finish < minimal_risk && lowest_risk_recorded > risk
+  #     queue << [
+  #       risk_min_until_finish*-1,
+  #       new_risk,
+  #       [y+1,x]
+  #     ]
+  #     lowest_risks[[y+1,x]] = new_risk
+  #   end
     
     
-  end
+  # end
 
-  if right
-    risk_min_until_finish = risk + right + distance(grid,y,x+1)
+  # if right
+  #   risk_min_until_finish = risk + right + distance(grid,y,x+1)
 
-    lowest_risk_recorded = lowest_risks[[y,x+1]] || 10000
-    new_risk = risk+right
+  #   lowest_risk_recorded = lowest_risks[[y,x+1]] || 10000
+  #   new_risk = risk+right
 
-    if risk_min_until_finish < minimal_risk && lowest_risk_recorded > risk
-      queue << [
-        risk_min_until_finish,
-        new_risk,
-        [y,x+1]
-      ]
-      lowest_risks[[y,x+1]] = new_risk
-    end
-  end  
+  #   if risk_min_until_finish < minimal_risk && lowest_risk_recorded > risk
+  #     queue << [
+  #       risk_min_until_finish*-1,
+  #       new_risk,
+  #       [y,x+1]
+  #     ]
+  #     lowest_risks[[y,x+1]] = new_risk
+  #   end
+  # end  
 
   # puts risk
     
-end
+# end
 
 
 
-puts minimal_risk
+# puts minimal_risk
